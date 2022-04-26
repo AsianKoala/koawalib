4fff381132376fc4256cad00a9d9bfec1ec54efa
Neil Mehra <neilmehra@outlook.com>
2022-04-26 03:45:23 -0400

    wait im stupid LMAO

--- koawalib/src/main/java/com/asiankoala/koawalib/command/group/DeadlineGroup.kt
+++ koawalib/src/main/java/com/asiankoala/koawalib/command/group/DeadlineGroup.kt
@@ -2,4 +2,4 @@ package com.asiankoala.koawalib.command.group
 
 import com.asiankoala.koawalib.command.commands.Command
 
-class DeadlineGroup(deadline: Command, vararg parallel: Command) : ParallelGroup({ deadline.isFinished }, *listOf(deadline, *parallel).toTypedArray())
+class DeadlineGroup(deadline: Command, vararg parallel: Command) : ParallelGroup({ deadline.isFinished }, deadline, *parallel)
