49aac6f6d47be7bc68267d393e22c347a15e8295
neil mehra <neilmehra@outlook.com>
2022-06-14 01:39:15 -0400

    added better error tracking to sequential command idx oob

--- koawalib/src/main/java/com/asiankoala/koawalib/command/group/SequentialGroup.kt
+++ koawalib/src/main/java/com/asiankoala/koawalib/command/group/SequentialGroup.kt
@@ -10,6 +10,9 @@ import com.asiankoala.koawalib.logger.Logger
 open class SequentialGroup(vararg commands: Command) : Command(), Group {
     private val cmds: MutableList<Command> = ArrayList()
     private var idx = -1
+    private var idxOnEnd = -1
+    private var hasInitialized = false
+    private var hasEnded = false
 
     final override fun addCommands(vararg commands: Command) {
         assert(idx == -1) { "Commands cannot be added to a CommandGroup while the group is running" }
@@ -19,9 +22,16 @@ open class SequentialGroup(vararg commands: Command) : Command(), Group {
     override fun initialize() {
         idx = 0
         cmds[0].initialize()
+        hasInitialized = true
     }
 
     override fun execute() {
+        if (!hasInitialized) {
+            throw Exception("sequential group $name has not initialized yet")
+        } else if (hasEnded) {
+            throw Exception("sequential group $name has ended on idx $idxOnEnd")
+        }
+
         var cmd = cmds[idx]
         cmd.execute()
         Logger.logDebug("command ${cmd.name} of group $name executed")
@@ -39,7 +49,9 @@ open class SequentialGroup(vararg commands: Command) : Command(), Group {
 
     override fun end() {
         if (idx in cmds.indices) cmds[idx].end()
+        idxOnEnd = idx
         idx = -1
+        hasEnded = true
     }
 
     override val isFinished: Boolean
