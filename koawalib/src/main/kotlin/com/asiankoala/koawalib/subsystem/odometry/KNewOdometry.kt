845792b0ea388ca064101e83f38c0e47b8330937
neil mehra <neilmehra@outlook.com>
2022-08-03 17:46:39 -0400

    meeowww

--- koawalib/src/main/kotlin/com/asiankoala/koawalib/subsystem/odometry/KNewOdometry.kt
+++ koawalib/src/main/kotlin/com/asiankoala/koawalib/subsystem/odometry/KNewOdometry.kt
@@ -5,18 +5,18 @@ import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer
 import com.asiankoala.koawalib.hardware.motor.KEncoder
 import com.asiankoala.koawalib.math.Pose
 
-class KRoadRunnerOdometry(
+class KNewOdometry(
     private val leftEncoder: KEncoder,
     private val rightEncoder: KEncoder,
     private val auxEncoder: KEncoder,
-    private val LATERAL_DISTANCE: Double,
-    private val FORWARD_OFFSET: Double
+    private val TRACK_WIDTH: Double,
+    private val PERP_TRACKER: Double
 ) : Odometry() {
     private val rrOdo = object : ThreeTrackingWheelLocalizer(
         listOf(
-            Pose2d(0.0, LATERAL_DISTANCE / 2, 0.0),
-            Pose2d(0.0, -LATERAL_DISTANCE / 2, 0.0),
-            Pose2d(FORWARD_OFFSET, 0.0, Math.toRadians(90.0))
+            Pose2d(0.0, TRACK_WIDTH / 2, 0.0),
+            Pose2d(0.0, -TRACK_WIDTH / 2, 0.0),
+            Pose2d(PERP_TRACKER, 0.0, Math.toRadians(90.0))
         )
     ) {
         override fun getWheelPositions(): List<Double> {
