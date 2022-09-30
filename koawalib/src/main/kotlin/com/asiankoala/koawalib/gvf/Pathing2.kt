a026c0c047d1c98a15591fcdf2eb28484de61270
neil mehra <neilmehra@outlook.com>
2022-09-30 17:13:27 -0400

    zz

--- koawalib/src/main/kotlin/com/asiankoala/koawalib/gvf/Pathing2.kt
+++ koawalib/src/main/kotlin/com/asiankoala/koawalib/gvf/Pathing2.kt
@@ -5,17 +5,11 @@ import kotlin.math.pow
 
 object Pathing2 {
     data class DifferentiablePoint(
-        val x: Double = 0.0,
+        val zero: Double = 0.0,
         val first: Double = 0.0,
         val second: Double = 0.0,
         val third: Double = 0.0
-    ) {
-        val l = listOf(x, first, second, third)
-    }
-
-    fun Pair<DifferentiablePoint, DifferentiablePoint>.vectorize(): List<Vector> {
-        return first.l.zip(second.l).map { Vector(it.first, it.second) }
-    }
+    )
 
     abstract class DifferentiableFunction {
         abstract operator fun get(t: Double): DifferentiablePoint
@@ -40,7 +34,6 @@ object Pathing2 {
         init {
             /**
              * https://www.wolframalpha.com/input?i=row+echelon+form+%5B%5B0%2C0%2C0%2C0%2C0%2Cf%2Cu%5D%2C%5B0%2C0%2C0%2C0%2Ce%2C0%2Cv%5D%2C%5B0%2C0%2C0%2C2d%2C0%2C0%2Cw%5D%2C%5Ba%2Cb%2Cc%2Cd%2Ce%2Cf%2Cx%5D%2C%5B5a%2C4b%2C3c%2C2d%2C1e%2C0%2Cy%5D%2C%5B20a%2C12b%2C6c%2C2d%2C0%2C0%2Cz%5D%5D
-             *
              * tldr to find coeffs, just shove it into a row echelon form
              * and then just calc it
              */
@@ -52,7 +45,7 @@ object Pathing2 {
             // 2 = 2u/f
             // 2f = 2u
             // f = u
-            coeffs[5] = start.x
+            coeffs[5] = start.zero
 
             // 2 = 2v/e
             // 2e = 2v
@@ -66,28 +59,34 @@ object Pathing2 {
 
             // 2 = -(20u + 12v + 3w - 20x + 8y - z) / c
             // c = -(20u + 12v + 3w - 20x + 8y - z) / 2
-            coeffs[2] = -(20 * start.x + 12 * start.first + 3 * start.second
+            coeffs[2] = -(20 * start.zero + 12 * start.first + 3 * start.second
                     - 20 * end.first + 8 * end.second - end.second) / 2.0
 
             // 2 = (30u + 16v + 3w -30x + 14y - 2z) / b
             // b = (30u + 16v + 3w -30x + 14y - 2z) / 2
-            coeffs[1] = (30 * start.x + 16 * start.first + 3 * start.second
-                    - 30 * end.x + 14 * end.first - 2 * end.second) / 2.0
+            coeffs[1] = (30 * start.zero + 16 * start.first + 3 * start.second
+                    - 30 * end.zero + 14 * end.first - 2 * end.second) / 2.0
 
             // 2 = -(12u + 6v + w - 12x + 6y -z) / a
             // a = -(12u + 6v + w - 12x + 6y -z) / 2
-            coeffs[0] = -(12 * start.x + 6 * start.first + start.second
-                    - 12 * end.x + 6 * end.first - end.second) / 2.0
+            coeffs[0] = -(12 * start.zero + 6 * start.first + start.second
+                    - 12 * end.zero + 6 * end.first - end.second) / 2.0
         }
     }
 
-    // used to represent unknown parameter
     abstract class Parametric(
         private val x: DifferentiableFunction,
         private val y: DifferentiableFunction
     ) {
-        open operator fun get(t: Double): List<Vector> {
-            return Pair(x[t], y[t]).vectorize()
+        open operator fun get(t: Double, n: Int = 0): Vector {
+            val xt = x[t]
+            val yt = y[t]
+            return when(n) {
+                1 -> Vector(xt.first, yt.first)
+                2 -> Vector(xt.second, yt.second)
+                3 -> Vector(xt.third, yt.third)
+                else -> Vector(xt.zero, yt.zero)
+            }
         }
 
         abstract val length: Double
@@ -107,20 +106,81 @@ object Pathing2 {
         val abscissa: Double
     )
 
+    abstract class GaussianQuadratureTable {
+        abstract val list: List<GaussianLegendreCoefficients>
+    }
+
+    class FivePointGaussianLegendre : GaussianQuadratureTable() {
+        override val list: List<GaussianLegendreCoefficients>
+            get() = listOf(
+                GaussianLegendreCoefficients(0.5688888888888889, 0.0000000000000000),
+                GaussianLegendreCoefficients(0.4786286704993665, -0.5384693101056831),
+                GaussianLegendreCoefficients(0.4786286704993665, 0.5384693101056831),
+                GaussianLegendreCoefficients(0.2369268850561891, -0.9061798459386640),
+                GaussianLegendreCoefficients(0.2369268850561891, 0.9061798459386640)
+            )
+    }
+
     class GaussianQuadrature(
         curve: Parametric,
-        vararg table: GaussianLegendreCoefficients
+        table: GaussianQuadratureTable
     ) {
         var length = 0.0
             private set
 
         init {
-            for(coefficient in table) {
+            for(coefficient in table.list) {
                 val t = 0.5 * (1.0 + coefficient.abscissa) // used for converting from [-1,1] to [0,1]
-                length += curve[t][1].norm * coefficient.weight
+                length += curve[t, 1].norm * coefficient.weight
             }
             length *= 0.5
         }
     }
 
+    open class Spline(
+        x: DifferentiableFunction,
+        y: DifferentiableFunction
+    ) : Parametric(x, y) {
+        private var _length: Double = 0.0
+        override val length: Double
+            get() = _length
+
+        init {
+            val gaussianQ = GaussianQuadrature(this, FivePointGaussianLegendre())
+            _length = gaussianQ.length
+        }
+    }
+
+    class SplineWithHeading(
+        x: DifferentiableFunction,
+        y: DifferentiableFunction
+    ) : Spline(x, y) {
+        fun heading(t: Double, n: Int): Double {
+            return when(n) {
+                0 -> this[t, 0].angle
+                else -> this[t, n].cross(this[t, n+1])
+            }
+        }
+    }
+
+    class Path(
+        private val splines: List<SplineWithHeading>
+    ) {
+        val length: Double = splines.sumOf { it.length }
+
+        fun s
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
