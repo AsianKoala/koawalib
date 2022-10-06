df87eda8b96c09d5376f5090103f3122bca1d051
neil mehra <neilmehra@outlook.com>
2022-10-06 18:45:41 -0400

    fixed invArc() not inverse arc'ing

--- koawalib/src/main/kotlin/com/asiankoala/koawalib/path/Testing.kt
+++ koawalib/src/main/kotlin/com/asiankoala/koawalib/path/Testing.kt
@@ -1,12 +1,18 @@
 package com.asiankoala.koawalib.path
 
 import com.asiankoala.koawalib.math.Pose
+import com.asiankoala.koawalib.math.Vector
+import com.asiankoala.koawalib.math.d
 import com.asiankoala.koawalib.math.radians
 
 object Testing {
     @JvmStatic
     fun main(args: Array<String>) {
         val t = Path(Pose(), Pose(20.0, 20.0, 90.0.radians))
-        println(t[t.length / 2.0])
+        println(t[0.0])
+        for(i in 0..t.length.toInt()) {
+            println(t[i.d])
+        }
+        println(t[t.length])
     }
 }
