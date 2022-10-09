3ef9b744dd633133a1edea1e67837ae5fb7b6709
neil mehra <neilmehra@outlook.com>
2022-10-09 18:25:11 -0400

    fixed path not finding a get for high lenghts

--- koawalib/src/main/kotlin/com/asiankoala/koawalib/path/Pathing.kt
+++ koawalib/src/main/kotlin/com/asiankoala/koawalib/path/Pathing.kt
@@ -490,7 +490,7 @@ abstract class Path(poses: List<Pose>) {
 
     operator fun get(s: Double, n: Int = 0): Pose {
         if(s <= 0.0) return curveSegments[0][0.0, n]
-        if(s > length) return this[length - 0.0001, n]
+        if(s >= length) return curveSegments[curveSegments.size-1][curveSegments[curveSegments.size-1].length, n] 
         curveSegments.fold(0.0) { acc, spline ->
             if(acc + spline.length > s) {
                 return spline[s - acc, n]
