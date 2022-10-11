8df7b3e2df843fda4829f30a02964e1d0293a6f0
neil mehra <neilmehra@outlook.com>
2022-10-11 17:53:38 -0400

    fixed triangle profile messing up on non-zero start position

--- koawalib/src/main/kotlin/com/asiankoala/koawalib/control/profile/MotionProfile.kt
+++ koawalib/src/main/kotlin/com/asiankoala/koawalib/control/profile/MotionProfile.kt
@@ -57,14 +57,15 @@ class MotionProfile(vararg _periods: MotionPeriod, reversed: Boolean) {
             val deccelStartState = end.copy(a = constraints.deccel)[-deccelTime]
             var deccelPeriod = MotionPeriod(deccelStartState, deccelTime)
 
-            val cruiseDt = ((end.x - start.x).absoluteValue - accelPeriod.dx - deccelPeriod.dx) / constraints.cruiseVel
+            val dx = (end.x - start.x).absoluteValue
+            val cruiseDt = (dx - accelPeriod.dx - deccelPeriod.dx) / constraints.cruiseVel
             var cruisePeriod = MotionPeriod(accelPeriod.endState.copy(a = 0.0), cruiseDt)
 
             if(cruiseDt < 0.0) {
                 Logger.logInfo("profile wont reach cruise vel")
                 cruisePeriod = cruisePeriod.copy(dt = 0.0)
                 val newA = min(constraints.accel.absoluteValue, constraints.deccel.absoluteValue)
-                val dt = sqrt((end.x / newA).absoluteValue)
+                val dt = sqrt(dx / newA)
                 accelPeriod = MotionPeriod(start.copy(a = newA), dt)
                 deccelPeriod = MotionPeriod(accelPeriod.endState.copy(a = -newA), dt)
             }
