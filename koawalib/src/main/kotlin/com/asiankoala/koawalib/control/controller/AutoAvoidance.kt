9f4af16c1dfb851c61bf89d4c71808f3490d6071
neil mehra <neilmehra@outlook.com>
2022-10-16 20:59:02 -0400

    ok fixed auto avoidance actually

--- koawalib/src/main/kotlin/com/asiankoala/koawalib/control/controller/AutoAvoidance.kt
+++ koawalib/src/main/kotlin/com/asiankoala/koawalib/control/controller/AutoAvoidance.kt
@@ -4,10 +4,11 @@ import com.asiankoala.koawalib.math.Vector
 import kotlin.math.PI
 
 // see my desmos graph explaining how this works
-// https://www.desmos.com/calculator/dav8kppwqr
+// https://www.desmos.com/calculator/px3tbum5kb 
 class AutoAvoidance(
     private val obstacles: List<Vector>,
     private val R: Double = 1.0,
+    private val kN: Double = 1.0,
     private val ps: () -> Vector,
 ) {
     fun calculate(i: Vector): Vector {
@@ -18,14 +19,14 @@ class AutoAvoidance(
         val theta = PI / 2.0
         val d1 = r dot v1
         val rm = r.norm
-        val a = 1.0 / rm
+        val a = kN / rm
         val n1 = r.rotate(theta)
         val d2 = v1 dot n1
         val n2 = r.rotate(if(d2 >= 0) theta else -theta)
-        val v2 = if(d1 >= 0.0 && r.norm > R) {
+        val v2 = if(d1 >= 0.0 || r.norm > R) {
             v1
         } else {
-            n2 - r * a
+            n2 + r * a
         }
         val im = i.norm
         val u = v2.unit
