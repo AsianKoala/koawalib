d90ab15a71c530524ea65a9e0cfddbd022f460dc
neil mehra <neilmehra@outlook.com>
2022-11-09 18:48:33 -0500

    neovim moment

--- core/src/main/kotlin/com/asiankoala/koawalib/hardware/sensor/KDistanceSensor.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/hardware/sensor/KDistanceSensor.kt
@@ -1,7 +1,7 @@
 package com.asiankoala.koawalib.hardware.sensor
 
-import com.asiankoala.koawalib.control.filter.RateLimiter
 import com.asiankoala.koawalib.hardware.KDevice
+import com.asiankoala.koawalib.util.Clock
 import com.asiankoala.koawalib.util.Periodic
 import com.qualcomm.robotcore.hardware.DistanceSensor
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
