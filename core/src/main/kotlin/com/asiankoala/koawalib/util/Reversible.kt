a7d0d4530df2b4d7c8f1934d1852a545035f01b0
neil mehra <neilmehra@outlook.com>
2022-11-09 13:09:11 -0500

    make reversible compatible with dash

--- core/src/main/kotlin/com/asiankoala/koawalib/util/Reversible.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/util/Reversible.kt
@@ -1,8 +1,8 @@
 package com.asiankoala.koawalib.util
 
-class Reversible<T>(
-    private val value: T,
-    private val revValue: T
+data class Reversible<T>(
+    @JvmField var value: T,
+    @JvmField var revValue: T
 ) {
     operator fun get(reversed: Boolean): T {
         return if (reversed) revValue else value
