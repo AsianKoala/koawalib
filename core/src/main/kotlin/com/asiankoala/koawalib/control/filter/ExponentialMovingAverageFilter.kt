d210a1d4a9cefa123612a6a6a1e18868747d0bc8
neil mehra <neilmehra@outlook.com>
2022-11-09 14:08:52 -0500

    zz

--- core/src/main/kotlin/com/asiankoala/koawalib/control/filter/ExponentialMovingAverageFilter.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/control/filter/ExponentialMovingAverageFilter.kt
@@ -1,14 +1,17 @@
 package com.asiankoala.koawalib.control.filter
 
+import java.util.*
 import kotlin.math.pow
 
 class ExponentialMovingAverageFilter(
     private val n: Int,
     private val alpha: Double = 2.0 / (n + 1.0)
 ) {
-    private val inputs = listOf<Double>()
+    private val inputs: Queue<Double> = LinkedList()
 
     private fun update(input: Double): Double {
+        inputs.add(input)
+        if(inputs.size > n + 1) inputs.remove()
         val sum = inputs.zip(List(n) { (1.0 - alpha).pow(it) }).sumOf { it.first * it.second }
         val weight = alpha *  List(n) { (1.0 - alpha).pow(n + it) }.sum()
         return sum / weight
