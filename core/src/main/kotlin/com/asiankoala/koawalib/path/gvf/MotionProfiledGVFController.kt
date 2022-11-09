8e5695919e77939b9990269696d0367a8a11def1
neil mehra <neilmehra@outlook.com>
2022-11-08 23:27:22 -0500

    motion profiled gvf works woo

--- core/src/main/kotlin/com/asiankoala/koawalib/path/gvf/MotionProfiledGVFController.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/path/gvf/MotionProfiledGVFController.kt
@@ -6,15 +6,12 @@ import com.asiankoala.koawalib.math.Vector
 import com.asiankoala.koawalib.math.angleWrap
 import com.asiankoala.koawalib.math.degrees
 import com.asiankoala.koawalib.path.Path
-import com.asiankoala.koawalib.subsystem.drive.KMecanumDrive
 import com.asiankoala.koawalib.util.Speeds
 import kotlin.math.PI
 import kotlin.math.absoluteValue
 import kotlin.math.pow
 import kotlin.math.sign
 
-// pretty mid motion profiled gvf
-// NEEDS TESTING!!!
 class MotionProfiledGVFController(
     private val path: Path,
     private val kN: Double,
@@ -37,7 +34,7 @@ class MotionProfiledGVFController(
     )
     private var state = DispState()
     private var normal = Vector()
-    private var error = 0.0
+    private var trackingError = 0.0
     private var headingError = 0.0
 
     override val isFinished: Boolean
@@ -49,14 +46,9 @@ class MotionProfiledGVFController(
         val tangent = path[s, 1].vec
         normal = tangent.rotate(PI / 2.0)
         val displacementVec = path[s].vec - pose.vec
-        error = displacementVec.norm * (displacementVec cross tangent).sign
-        Logger.logInfo("s: $s, d: $tangent, r: $displacementVec, e: $error")
-        return tangent - normal * kN * errorMap.invoke(error)
+        trackingError = displacementVec.norm * (displacementVec cross tangent).sign
+        return tangent - normal * kN * errorMap.invoke(trackingError)
     }
-
-    // basic feedforward
-    private fun feedforwardMap(velAccel: Pair<Double, Double>) =
-        kStatic * velAccel.first.sign + velAccel.first * kV + velAccel.second * kA
 
     // the vector triple product BAC-CAB identity
     private fun tripleProduct(a: Vector, b: Vector, c: Vector) = b * (a dot c) - c * (a dot b)
@@ -66,7 +58,7 @@ class MotionProfiledGVFController(
     // deals with heading. just needs a bit of testing tbh
     private fun headingControl(): Pair<Double, Double> {
         headingError = (path[s].heading - pose.heading).angleWrap.degrees
-        return Pair(error / kOmega * state.v, error)
+        return Pair(headingError / kOmega * state.v, headingError)
     }
 
     // d/ds v(p(s)) = v'(p(s)) * p'(s)
@@ -76,28 +68,21 @@ class MotionProfiledGVFController(
     // bc im lazy lets just assume linear error map
     // so e' = 1.0
     // v' = t' - k_n * (n' * e + n)
+    // now plug this into d^2/ds^2 and we chilling
     private fun vectorControl(): Pair<Vector, Vector> {
-        val v = calcGVF()
-        val md = v.unit
-        Logger.addTelemetryData("md", md)
-        val vel = md * state.v
-        Logger.addTelemetryData("vel", vel)
-        val dvds = path[s, 2].vec - (path[s, 2].vec.rotate(PI / 2.0) * error + normal) * kN
-        // from https://math.stackexchange.com/questions/2983445/unit-vector-differentiation
-        val mdDot = tripleProduct(v, dvds, v) / v.norm.pow(3)
-        val accel = mdDot * state.v * state.v + md * state.a
+        val vs = calcGVF()
+        val ms = vs.unit
+        val dtds = path[s, 2].vec
+        val dvds = dtds - (dtds.rotate(PI / 2.0) * trackingError + normal) * kN
+        // from https://math.stackexchange.com/a/3826369
+        val dmds = tripleProduct(vs, dvds, vs) / vs.norm.pow(3)
+        val vel = ms * state.v
+        val accel = dmds * state.v * state.v + ms * state.a
         return Pair(vel, accel)
     }
 
-    private fun calcFeedforward(vel: Pose, accel: Pose): Pose {
-        val vels = KMecanumDrive.mecKinematics(vel)
-        Logger.addTelemetryData("vels", vels)
-        val accels = KMecanumDrive.mecKinematics(accel)
-        val powers = vels.zip(accels).map(::feedforwardMap) // kotlin syntax so clean
-        return Speeds()
-            .apply { setWheels(powers, pose.heading) }
-            .getRobotCentric(pose.heading)
-    }
+    private fun calcVecFF(vel: Vector, accel: Vector) = vel.unit * kStatic + vel * kV + accel * kA
+    private fun calcHeadingFF(vel: Double) = kStatic * vel.sign + vel * kV
 
     override fun update(currPose: Pose): Pose {
         pose = currPose
@@ -106,8 +91,16 @@ class MotionProfiledGVFController(
         if (kIsTuning) Logger.addVar("target velocity", state.v)
         val headingResult = headingControl()
         val vectorResult = vectorControl()
-        val vel = Pose(vectorResult.first, headingResult.first)
-        val accel = Pose(vectorResult.second, 0.0)
-        return calcFeedforward(vel, accel)
+        return Speeds().apply {
+            setFieldCentric(
+                Pose(
+                    calcVecFF(
+                        vectorResult.first,
+                        vectorResult.second
+                    ),
+                    calcHeadingFF(headingResult.first)
+                )
+            )
+        }.getRobotCentric(pose.heading)
     }
 }
