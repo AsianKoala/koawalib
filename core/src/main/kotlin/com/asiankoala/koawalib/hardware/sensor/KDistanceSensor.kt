ff2f5c183aab0e53e503fbcc2651ba4bb734a31a
neil mehra <neilmehra@outlook.com>
2022-11-09 15:58:07 -0500

    gvf cmd integration mid :pensive:

--- core/src/main/kotlin/com/asiankoala/koawalib/hardware/sensor/KDistanceSensor.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/hardware/sensor/KDistanceSensor.kt
@@ -10,11 +10,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
  * applies to the rev color sensor v3 and rev 2m distance sensor (any devices that implement DistanceSensor)
  */
 @Suppress("unused")
-class KDistanceSensor(name: String) : KDevice<DistanceSensor>(name), Periodic {
-    private var rateLimiter = RateLimiter(50.0) { lastRead = device.getDistance(DistanceUnit.MM) }
+class KDistanceSensor(
+    name: String, 
+    private val dt: Double
+) : KDevice<DistanceSensor>(name), Periodic {
+    private var lastReadTime = Clock.seconds
 
     var lastRead = Double.NaN
         private set
 
-    override fun periodic() = rateLimiter.periodic()
+    override fun periodic() {
+        val t = Clock.seconds
+        if(t - lastReadTime > dt) {
+            lastRead = device.getDistance(DistanceUnit.MM)
+            lastReadTime = t
+        }
+    }
 }
