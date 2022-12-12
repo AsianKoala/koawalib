ab3682c4fd061b9189321106fc8f13ed25b86aab
neil mehra <neilmehra@outlook.com>
2022-12-12 08:44:28 -0500

    wh

--- core/src/main/kotlin/com/asiankoala/koawalib/command/group/RaceGroup.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/command/group/RaceGroup.kt
@@ -2,4 +2,4 @@ package com.asiankoala.koawalib.command.group
 
 import com.asiankoala.koawalib.command.commands.Cmd
 
-class RaceGroup(vararg cmds: Cmd) : ParallelGroup({ it.containsValue(false) }, *cmds)
+open class RaceGroup(vararg cmds: Cmd) : ParallelGroup({ it.containsValue(false) }, *cmds)
