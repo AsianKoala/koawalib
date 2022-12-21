56daab7eccf493446c6697ea80a269794e9c3e33
neil mehra <neilmehra@outlook.com>
2022-12-21 00:48:36 -0500

    ktlint

--- core/src/main/kotlin/com/asiankoala/koawalib/control/filter/SlewRateLimiter.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/control/filter/SlewRateLimiter.kt
@@ -3,44 +3,21 @@ package com.asiankoala.koawalib.control.filter
 import com.asiankoala.koawalib.math.clamp
 import com.asiankoala.koawalib.util.Clock
 
-/**
- * A class that limits the rate of change of an input value. Useful for implementing voltage,
- * setpoint, and/or output ramps. A slew-rate limit is most appropriate when the quantity being
- * controlled is a velocity or a voltage
- *
- * note: ported from wpilib
- */
-class SlewRateLimiter @JvmOverloads constructor(
-    private val m_rateLimit: Double,
-    private var m_prevVal: Double = 0.0
+class SlewRateLimiter(
+    private val r: Double,
 ) {
-    private var m_prevTime: Double = Clock.seconds
+    private var ukm1 = 0.0
+    private var tkm1 = Clock.seconds
 
-    /**
-     * Filters the input to limit its slew rate.
-     *
-     * @param input The input value whose slew rate is to be limited.
-     * @return The filtered value, which will not change faster than the slew rate.
-     */
     fun calculate(input: Double): Double {
-        val currentTime: Double = Clock.seconds
-        val elapsedTime = currentTime - m_prevTime
-        m_prevVal += clamp(
-            input - m_prevVal,
-            -m_rateLimit * elapsedTime,
-            m_rateLimit * elapsedTime
-        )
-        m_prevTime = currentTime
-        return m_prevVal
+        val dt = Clock.seconds - tkm1
+        ukm1 += clamp(input - ukm1, -r * dt, r * dt)
+        tkm1 = Clock.seconds
+        return ukm1
     }
 
-    /**
-     * Resets the slew rate limiter to the specified value; ignores the rate limit when doing so.
-     *
-     * @param value The value to reset to.
-     */
-    fun reset(value: Double) {
-        m_prevVal = value
-        m_prevTime = Clock.seconds
+    fun reset(u: Double) {
+        ukm1 = u
+        tkm1 = Clock.seconds
     }
 }
