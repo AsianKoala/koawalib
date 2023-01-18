778e916ef82514e20afb133f748c5c5150f50b40
neil mehra <neilmehra@outlook.com>
2023-01-17 19:35:11 -0500

    doccing

--- core/src/main/kotlin/com/asiankoala/koawalib/gamepad/functionality/ButtonScheduling.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/gamepad/functionality/ButtonScheduling.kt
@@ -56,15 +56,28 @@ interface ButtonScheduling {
     }
 
     /**
-     * Schedule a command when toggled
+     * Schedule a command when just toggled
      * @param cmd command to schedule
      */
     fun onToggle(cmd: Cmd) {
         schedule(::isToggled, cmd)
     }
 
+    /**
+     * Schedule a command when just untoggled
+     * @param[cmd] command to schedule
+     */
     fun onUntoggle(cmd: Cmd) {
         schedule(::isUntoggled, cmd)
+    }
+
+    /**
+     * Schedule a command when an additional condition is met and button is just pressed
+     * @param[cmd] command to schedule
+     * @param[condition] additional condition required to schedule command
+     */
+    fun scheduleConditional(cmd: Cmd, condition: () -> Boolean) {
+        schedule({ isJustPressed && condition.invoke() }, cmd)
     }
 
     private fun schedule(condition: () -> Boolean, cmd: Cmd) {
